4959032d3205245be6ff463f4d9c278f30c5cf88
Philip Barrett <pobarrett@gmail.com>
2014-01-09 22:33:26 -0600

    Halfway through 2-dimensional approximation. Not fully working yet.

--- R/dn.R
+++ R/dn.R
@@ -7,7 +7,8 @@
 
 #' Mutli-dimensional Chebychev approximation
 #' 
-#' Standard Chebychev approximation of an arbitrary function. [INCOMPLETE]
+#' Standard Chebychev approximation of an arbitrary function. [INCOMPLETE -
+#' CURRENTLY AIMING ONLY FOR 2-DIMENSIONAL FUNCTIONS]
 #' 
 #' @param fn a function \eqn{f(x_1,..., x_n)} or \eqn{f(x_1, ..., x_n,\beta)} 
 #'   for \eqn{\beta} a list of function parameters.  If the latter, must be 
@@ -49,24 +50,27 @@ dn.poly <- function( fn, range, iOrder, iPts, fn.opts=NULL, fn.vals=NULL,
     else stop( 'Function options not yet available' ) # sapply( grid, fn, opts=fn.opts )
         # Computes the function over the grid if not already submitted, passing
         # function options if required
+  fn.vec <- as.vector( t( fn.vals ) )
+        # The vector of function values
   zz <- mapply( d1.grid, rep( list(c(-1,1)), iDim ), iPts )
         # Compress the grid into [-1,1]
-  
-  # Steps
-  #  1. Create polynomials for each one-dimension grid
-  #  2. Multiply them together to get the grid of polynomial basis values
-  #  3. Unlist both function values and polynoimal basis
-  #  4. Then regress one on the other
-  #  5. Convert to A "stndard polynomial
-  
-  basis.poly <- chebyshev.t.polynomials( iOrder, normalized=FALSE )
-        # A list of polynomial objects
-  basis.vals <- as.matrix( sapply( zz, function(z.pt) 
-    sapply( basis.poly, function( pol ) as.function( pol )( z.pt ) ) ) )
+  basis.poly <- lapply( iOrder, chebyshev.t.polynomials, normalized=FALSE )
+        # A list of (list of) polynomial objects
+  basis.vals <- lapply( 1:iDim, function(i) as.matrix( sapply( zz[[i]], function(z.pt) 
+                      sapply( basis.poly[[i]], function( pol ) as.function( pol )( z.pt ) ) ) ) )
         # The values of the basis on the grid
-  basis.reg <- lm( fn.vals ~ 0 + t( basis.vals ) )
+  
+  # FROM HERE ON 2 DIMENSIONS ONLY
+  basis.vec <- t( do.call( 'cbind', lapply( 1:iPts[1], function(i) apply( basis.vals[[2]], MARGIN=2, 
+                    function(col2) as.vector( outer( basis.vals[[1]][,i], col2 ) ) ) ) ) )
+        # A matrix values where each row is the value of the cross-product of
+        # the polynomials at each grid point
+  basis.reg <- lm( fn.vec ~ 0 + basis.vec )
         # The regression of the function evaluation points on the basis grid
         # (zero eliminates intercept)
+  
+  
+  # AFTER HERE MY BRAIN FAILED
   poly <- do.call( 'sum', lapply( 1:(iOrder+1), function(i) basis.reg$coef[i] * basis.poly[[i]] ) )
         # Construct a polynomial  representation of the function
   fn.poly <- function( x ) as.function(poly)( d1.normalize( x, range ) ) 
@@ -79,6 +83,8 @@ dn.poly <- function( fn, range, iOrder, iPts, fn.opts=NULL, fn.vals=NULL,
 # These two functions create a multi-dimensional version of outer
 # Taken from stackoverflow solution
 # http://stackoverflow.com/questions/6192848/how-to-generalize-outer-to-n-dimensions
+list_args <- Vectorize( function(a,b) c( as.list(a), as.list(b) ), 
+                        SIMPLIFY = FALSE)
 make_args_mtx <- function( alist )
   Reduce(function(x, y) outer(x, y, list_args), alist)
 multi.outer <- function(f, ... ) {
